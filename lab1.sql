3f21ee7d81ea76fab05b9905d8462b61b1fd24cd
Krzycholl18 <32575999+krzychsol@users.noreply.github.com>
2021-10-18 20:22:40 +0200

    Add files via upload

--- lab1.sql
+++ lab1.sql
@@ -45,3 +45,20 @@ odbiorcy jest Argentyna
 
 --SELECT OrderID,OrderDate,CustomerID FROM Orders WHERE (ShippedDate IS NULL ) AND ( ShipCountry = 'Argentina')
 
+/*ORDER BY
+1. Wybierz nazwy i kraje wszystkich klientów, wyniki posortuj wed³ug kraju, w
+ramach danego kraju nazwy firm posortuj alfabetycznie
+2. Wybierz informacjê o produktach (grupa, nazwa, cena), produkty posortuj wg
+grup a w grupach malej¹co wg ceny
+3. Wybierz nazwy i kraje wszystkich klientów maj¹cych siedziby w Japonii (Japan)
+lub we W³oszech (Italy), wyniki posortuj tak jak w pkt 1
+*/
+
+--1
+--SELECT Country,CompanyName FROM Customers ORDER BY Country,CompanyName
+--2
+--SELECT CategoryName,ProductName,UnitPrice FROM Products,Categories WHERE Categories.CategoryID = Products.CategoryID ORDER BY CategoryName,UnitPrice DESC
+--3
+--SELECT CompanyName,Country FROM Customers WHERE ( Country = 'Japan' ) OR ( Country = 'Italy' ) ORDER BY Country,CompanyName
+
+
