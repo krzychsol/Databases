7cff01005477e1ec54c2a4c934208d8235a742a7
ksolecki <32575999+krzychsol@users.noreply.github.com>
2021-12-04 16:02:37 +0100

    Update Hackerrank_hard_exercises.sql

--- Advanced_DQL_queries/Hackerrank_hard_exercises.sql
+++ Advanced_DQL_queries/Hackerrank_hard_exercises.sql
@@ -12,28 +12,35 @@ Uwaga: określony konkurs może służyć do sprawdzania kandydatów w więcej n
 
 */
 
+WITH sum_of_view_stats AS (
+        SELECT challenge_id,
+                total_views = SUM(total_views),
+                total_unique_views = sum(total_unique_views)
+        FROM View_Stats 
+        GROUP BY challenge_id)
+
+,sum_of_submission_stats AS (
+        SELECT challenge_id,
+                total_submissions = sum(total_submissions),
+                total_accepted_submissions = sum(total_accepted_submissions)
+        FROM Submission_Stats 
+        GROUP BY challenge_id)
+
 SELECT con.contest_id,
-        con.hacker_id, 
-        con.name, 
-        SUM(total_submissions), 
-        SUM(total_accepted_submissions), 
+        con.hacker_id,
+        con.name,
+        SUM(total_submissions),
+        SUM(total_accepted_submissions),
         SUM(total_views),
         SUM(total_unique_views)
-FROM contests con
-JOIN colleges col on con.contest_id = col.contest_id 
-JOIN challenges cha on  col.college_id = cha.college_id 
-LEFT JOIN (SELECT challenge_id,
-            SUM(total_views) AS sum_of_views,
-            SUM(total_unique_views) AS sum_of_unique_views
-            FROM view_stats 
-            GROUP BY challenge_id) vs ON cha.challenge_id = vs.challenge_id 
-LEFT JOIN (SELECT challenge_id, 
-            SUM(total_submissions) AS total_submissions,
-            SUM(total_accepted_submissions) AS total_accepted_submissions 
-            FROM submission_stats GROUP BY challenge_id) ss ON cha.challenge_id = ss.challenge_id
-GROUP BY con.contest_id, con.hacker_id, con.name
-HAVING  SUM(total_submissions)!=0 OR 
-        SUM(total_accepted_submissions)!=0 OR
-        SUM(total_views)!=0 OR
-        SUM(total_unique_views)!=0
-ORDER BY contest_id;
+FROM Contests con
+INNER JOIN Colleges col ON con.contest_id = col.contest_id
+INNER JOIN Challenges cha ON cha.college_id = col.college_id
+LEFT JOIN SUM_View_Stats vs ON vs.challenge_id = cha.challenge_id
+LEFT JOIN SUM_Submission_Stats ss ON ss.challenge_id = cha.challenge_id
+GROUP BY con.contest_id,con.hacker_id,con.name
+HAVING (SUM(total_submissions)
+        +SUM(total_accepted_submissions)
+        +SUM(total_views)
+        +SUM(total_unique_views)) <> 0
+ORDER BY con.contest_ID
